0c5e9b0a34e4e34bd112e4276dfea854fb86c0d6
Your Name <197651399+koti16@users.noreply.github.com>
2026-07-22 22:28:17 +0530

    Organize Java project into src folder

---
+++ src/DataTypes.java
@@ -0,0 +1,31 @@
+package src;
+
+public class DataTypes {
+    
+    byte  cat = 127;
+    int u = 1000;
+    short s = 10000;
+    float f = 10.5f;
+
+    public static void main(String[] args) {
+        System.out.println(new DataTypes().cat);
+        System.out.println(new DataTypes().u);
+        System.out.println(new DataTypes().s);
+        System.out.println(new DataTypes().f);
+        System.out.println(Integer.SIZE);
+        System.out.println(Integer.MAX_VALUE);
+        System.out.println(Integer.MIN_VALUE);
+        System.out.println(Byte.SIZE);
+        System.out.println(Byte.MAX_VALUE);
+        System.out.println(Byte.MIN_VALUE);
+        System.out.println(Short.SIZE);
+        System.out.println(Short.MAX_VALUE);
+        System.out.println(Short.MIN_VALUE);
+        System.out.println(Float.SIZE);
+        System.out.println(Float.MAX_VALUE);
+        System.out.println(Float.MIN_VALUE);
+
+    }
+}
+
+
